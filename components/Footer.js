2335cfe3787e56b11011630ea930113b691daf34
callsmasterofficial <partnerchc@outlook.com>
2022-05-04 17:36:36 +0530

    popular-search-add

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ import siteConfig from '../siteConfig'
 
 function Footer() {
   const router = useRouter()
- 
+  
   return (
     <div className='footer-container mt-[20%] w-screen md:mt-[10%]'>
        <div className='footer-box border-t-2 border-white -mt-14'></div>
@@ -24,17 +24,26 @@ function Footer() {
             <InstallButton/>
           </div>
           
-         <button className='btn mb-5 -mt-6 md:mt-0 p-2 w-auto text-xl flex items-center justify-center' >
-           <span className='font-semibold px-2' onClick={()=>router.push("/blog")}>Blog</span>
+         <button className='bg-red-700 rounded-[4px] mb-5 -mt-6 md:mt-0 p-2 w-auto text-xl flex items-center justify-center' >
+           <span className='font-bold px-2 text-base text-white ' onClick={()=>router.push("/blog")}>Blog</span>
          </button>
          
        </div>
+          <div className='popu_search_container mb-4'>
+              <div className='popu_search_box  p-2 flex items-center justify-center -mt-6 '>
+                 <span className='span  flex items-center'><img className='w-8 h-8 pr-2 object-contain' src="https://img.icons8.com/ios-glyphs/344/ffffff/right3.png" alt="side_icon" />
+                 <h2 className='text-white font-bold mr-1'>Popular Searches</h2><span className='mr-1 text-white'>:</span><p className='font-semibold text-base text-white'>HBO Watch Party, Watch Party HBO Max, HBO Go Watch Party, HBO Max Watch Party, HBO Party, HBO Max Party
+                 </p></span>
+              </div>
 
-        <div className='select-box flex items-center justify-center'>
-          <select name="" id="">
-            <option value="">English</option>
-          </select>
-        </div>
+              <div className='popu_search_box  p-2 flex items-center justify-center'>
+                 <span className='span  flex items-center'>
+                 <h2 className='text-white font-bold mr-1 text-base'>Disclaimer</h2><span className='mr-1 text-white'>:</span><p className='font-semibold text-base text-white'>HBO is a trademark of Home Box Office, Inc.. This website & extension hold no association or affiliation with HBO Max or any third-party companies.
+                 </p></span>
+              </div>
+
+    
+          </div>
     </div>
   )
 }
--- pages/blogmore.js
+++ pages/blogmore.js
@@ -3,10 +3,12 @@ import Navbar from '../components/Navbar'
 
 import Blogmore from '../components/Blogmore'
 import Footer from '../components/Footer'
+import Banner from '../components/Banner'
 function blogmore() {
   return (
     <div className='blogmore'>
       <Navbar/>
+      <Banner/>
       <Blogmore/>
       <Footer/>
     </div>
